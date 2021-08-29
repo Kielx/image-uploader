815f3eeb8e5d918d40315d5786f8e079797fcf85
Kielx <kielx.dev@gmail.com>
2021-08-29 21:28:32 +0200

    Add firebase initial file uploader

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,6 +1,12 @@
 import React from "react";
 import image from "../assets/images/image.svg";
 import { useDropzone } from "react-dropzone";
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage";
 
 const ImageUploader = (props) => {
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
@@ -13,6 +19,68 @@ const ImageUploader = (props) => {
       {file.path} - {(file.size / 1000000).toFixed(2)} MB
     </span>
   ));
+
+  const upload = (file) => {
+    const storage = getStorage();
+    console.log(file);
+
+    // Create the file metadata
+    /** @type {any} */
+    const metadata = {
+      contentType: "image/jpeg",
+    };
+
+    // Upload file and metadata to the object 'images/mountains.jpg'
+    const storageRef = ref(storage, `image-uploader/${file.name}`);
+    const uploadTask = uploadBytesResumable(storageRef, file, metadata);
+
+    // Listen for state changes, errors, and completion of the upload.
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log("Upload is " + progress + "% done");
+        switch (snapshot.state) {
+          case "paused":
+            console.log("Upload is paused");
+            break;
+          case "running":
+            console.log("Upload is running");
+            break;
+          default:
+            break;
+        }
+      },
+      (error) => {
+        // A full list of error codes is available at
+        // https://firebase.google.com/docs/storage/web/handle-errors
+        switch (error.code) {
+          case "storage/unauthorized":
+            // User doesn't have permission to access the object
+            break;
+          case "storage/canceled":
+            // User canceled the upload
+            break;
+
+          // ...
+
+          case "storage/unknown":
+            // Unknown error occurred, inspect error.serverResponse
+            break;
+          default:
+            break;
+        }
+      },
+      () => {
+        // Upload completed successfully, now we can get the download URL
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          console.log("File available at", downloadURL);
+        });
+      }
+    );
+  };
 
   return (
     <div className="flex flex-col space-y-6 items-center w-full max-w-md shadow-md bg-white rounded-[12px] m-auto py-12 px-8">
@@ -41,7 +109,10 @@ const ImageUploader = (props) => {
       </div>
       <input {...getInputProps()} />
       {acceptedFiles.length > 0 ? (
-        <button className="text-center bg-green-500 hover:bg-green-600 transition-all rounded-[8px] p-3 text-white">
+        <button
+          onClick={() => upload(acceptedFiles[0])}
+          className="text-center bg-green-500 hover:bg-green-600 transition-all rounded-[8px] p-3 text-white"
+        >
           Upload file
         </button>
       ) : (
