8ecf40269d8fd89de58001dede52fba306959c47
Kielx <kiel66@gmail.com>
2021-09-02 22:23:31 +0200

    Fix manifest error

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -17,6 +17,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const firebase = initializeApp(firebaseConfig);
 
+//MainFirebase END
+
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
 const appCheck = initializeAppCheck(firebase, {
